3c5d7890df5b8bdbb23b2b8c5b54dd5f00da6319
Richard Ioki <richie.ioki@gmail.com>
2019-03-15 17:07:58 -0700

    Sending messages through and getting jsons

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
+import java.io.IOException;
+
+import Torobts.ELO.TBAConnector;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		/*TBAConnector connection = new TBAConnector();
+		TBAConnector connection = new TBAConnector();
 		
 		try {
-			//connection.run("https://www.thebluealliance.com/api/v3");
+			connection.run("https://www.thebluealliance.com/api/v3/team/frc1197/events/simple");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}*/
+		}
 	}
 
 }
--- src/Torobts/ELO/TBAConnector.java
+++ src/Torobts/ELO/TBAConnector.java
@@ -1,6 +1,10 @@
 package Torobts.ELO;
 
 import java.io.IOException;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import okhttp3.*;
 
 public class TBAConnector {
@@ -8,17 +12,26 @@ public class TBAConnector {
 	
 	public String run(String url) throws IOException {
 		Request request = new Request.Builder() 
-				.url("https://www.thebluealliance.com/api/v3")
+				.url(url)
+				.addHeader("X-TBA-Auth-Key", "LvUE8eCp0y16S55LeSxm44uNUQqgsqdgIHkEraPD3UySNx92S4e9XLR2cm07l2FQ")
 				.build();
 			
 		Response response = client.newCall(request).execute();
-		System.out.println(response.toString());
+		String jsonraw = response.body().string();
+		System.out.println(jsonraw);
+		//jsonraw = jsonraw.substring(1, jsonraw.length() - 1);
+		//jsonraw = jsonraw.trim();
+		JSONArray json = new JSONArray(jsonraw);
+		JSONObject one = (JSONObject) json.get(0);
+		
+		//JSONArray Cities = json.getJSONArray("city");
+		System.out.println(one.get("city"));
+		
+		
 		return response.toString();
 	}
 	
-	public TBAConnector() {	
-		
-		
-	
+	public TBAConnector() {			
+			
 	}
 }
